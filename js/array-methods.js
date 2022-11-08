7724308dfe015235cf55378709f65c188d523e11
mariabragado <mjbragado@gmail.com>
2022-11-08 20:01:47 +0100

    fixed bug detected by Sonar

--- js/array-methods.js
+++ js/array-methods.js
@@ -1,8 +1,12 @@
 export const calculateLength = (array) => {
-    if (array === undefined) throw new RangeError('Parámetro no incluido');
+    caseError(array);
     let counter = 0;
     for (let i = 0; array[i] !== undefined; i++) {
         counter++;
     }
-    return counter++;
+    return counter;
+};
+
+export const caseError = (array) => {
+    if (array === undefined) throw new RangeError('Parámetro no incluido');
 };
